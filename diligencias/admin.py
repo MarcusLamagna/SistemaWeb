adccfc5ca9387a36af19abd08bf0ddcae80acbfd
MLamagna <marcus@mlamagna.com.br>
2023-08-19 15:31:31 -0300

    Adição de novos códigos

--- diligencias/admin.py
+++ diligencias/admin.py
@@ -2,5 +2,14 @@ from django.contrib import admin
 
 from diligencias.models import Diligencia
 
+
+# Cruiando a classe DiligenciaAdmin
+class DiligenciaAfmin(admin.ModelAdmin):
+    # quais campso da classe serão exibidos
+    list_display = ['nome', 'data_realizacao', 'dinheiro_apreendido']
+    search_fields = ['nome']
+    list_filter = ['data_realizacao']
+
+
 # Register your models here.
-admin.site.register(Diligencia)
+admin.site.register(Diligencia, DiligenciaAfmin)
